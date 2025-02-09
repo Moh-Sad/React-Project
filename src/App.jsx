16227c449637422f5bf06514f7e37678da1d75a7
Mohammed Sadik <codeprogrammer111@gmail.com>
2025-02-09 23:46:16 +0300

    refactor: remove Tailwind CSS import from App.css, update index.css, and add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-        <h1 className="text-3xl font-bold text-center mt-5">Hello, Tailwind CSS!</h1>
-        <p className="text-center mt-5">This is a React app with Tailwind CSS.</p>
+      <Navbar />
     </div>
   )
 }
