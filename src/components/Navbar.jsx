cd21ba21733f6f40148490dbb2d25411b3babd66
Mohammed Sadik <codeprogrammer111@gmail.com>
2025-04-02 12:11:23 +0300

    fix: update Navbar component for improved responsiveness and toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
@@ -11,7 +10,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between item-center h-24 max-w-[1240px] mx-auto p-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
-      <ul className="flex hidden">
+      <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
@@ -19,12 +18,20 @@ const Navbar = () => {
         <li className="p-4">Contact</li>
       </ul>
 
-      <div>
-        <AiOutlineMenu size={30} />
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
       </div>
 
-      <div className="fixed pt-4 left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500">
-      <h1 className="w-full pb-10 text-3xl font-bold text-[#00df9a]">REACT.</h1>
+      <div
+        className={
+          !nav
+            ? "fixed p-4 left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[-100%] ease-in-out duration-500"
+        }
+      >
+        <h1 className="w-full pb-10 text-3xl font-bold text-[#00df9a]">
+          REACT.
+        </h1>
         <ul className="uppercase p-4">
           <li className="p-4 border-b border-gray-600">Home</li>
           <li className="p-4 border-b border-gray-600">Company</li>
